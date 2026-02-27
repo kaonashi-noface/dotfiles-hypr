9da137f819f108079d32726faf3b79f548d59f17
kaonashi-noface <dev@paulckim.com>
2026-02-27 06:19:26 -0800

    remove custom rice related packages

--- install.sh
+++ install.sh
@@ -4,13 +4,10 @@
 pacman -S --needed base-devel \
     linux-headers linux-lts-headers \
     nvidia-utils nvidia-settings nvidia-dkms \
-    sddm xorg-server xorg-xinit \
-    qt6-declarative qt5-declarative qt6-5compat \
-    qt5-graphicaleffects qt5-quickcontrols2 \
-    git zsh nvim less tree opencode \
+    git zsh nvim less tree \
     rustup nvm lua \
-    yazi ffmpeg 7zip jq poppler fd ripgrep fzf zoxide resvg imagemagick \
-    ttf-meslo-nerd
+    opencode \
+    ttf-meslo-nerd ttf-fira-code
 
 # Install the lastest stable version of rust
 rustup default stable
@@ -21,7 +18,7 @@ nvm alias default "lts/*"
 # TODO: add paru compilation steps here later...
 
 
-paru -S brave-bin wezterm-git
+yay -S brave-bin wezterm-git
 
 
 # TODO git clone where is my sddm theme
