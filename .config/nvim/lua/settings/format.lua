465506a4da9bf1a640c05d46e23bbd43c2991176
kaonashi-noface <dev@paulckim.com>
2026-02-26 23:29:04 -0800

    replace tab with 4 spaces for indent

---
+++ .config/nvim/lua/settings/format.lua
@@ -0,0 +1,5 @@
+vim.opt.tabstop = 4        -- A tab displays as 4 spaces
+vim.opt.shiftwidth = 4     -- Indentation width for >>, <<, and auto-indent
+vim.opt.expandtab = true   -- Insert spaces instead of tab characters
+vim.opt.softtabstop = 4    -- Number of spaces inserted/deleted with <Tab> and <BS>
+
