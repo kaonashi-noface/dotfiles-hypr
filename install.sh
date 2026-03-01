222a1a14fce3924132604a9e5fdba04513239b40
no_face <dev@paulckim.com>
2026-03-01 07:22:55 -0800

    update install script pacman & paru packages to reflect DMS & Silent SDDM setup

--- install.sh
+++ install.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 # Install all dependencies
-pacman -S --needed base-devel \
-    linux-headers linux-lts-headers \
-    nvidia-utils nvidia-settings nvidia-dkms \
-    git zsh nvim less tree \
+pacman -S --needed base-devel linux-headers linux-lts-headers \
+    nvidia-utils nvidia-settings mesa-utils nvidia-dkms \
+    xorg-server xorg-xinit sddm \
+    qt6-base qt6-declarative qt6-tools \
+    qt6-svg qt6-wayland qt6-translations \
+    zsh nvim less tree yazi zip unzip \
     rustup nvm lua \
-    opencode \
+    git github-cli cava opencode \
     ttf-meslo-nerd ttf-fira-code
+
+# sudo pacman -S sddm xorg-server xorg-xinit
+
+# sudo pacman -S qt6-base qt6-declarative qt6-tools qt6-svg qt6-translations qt6-wayland
 
 # Install the lastest stable version of rust
 rustup default stable
@@ -15,13 +21,15 @@ rustup default stable
 nvm install --lts
 nvm alias default "lts/*"
 
-# TODO: add paru compilation steps here later...
+# TODO: add proper pushd steps
+git clone https://aur.archlinux.org/paru.git
+cd paru # replace with pushd
+makepkg -si
+# TODO: add proper popd & clean up steps
+
+paru -S brave-bin
 
 
-yay -S brave-bin wezterm-git
-
-
-# TODO git clone where is my sddm theme
-# setup sddm, theme & x11 configs
+# TODO: automate everything else...
 
 
