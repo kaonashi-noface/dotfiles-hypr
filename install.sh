d93c2b46ae04191477ea5ff538c60326dbda61d2
no_face <dev@paulckim.com>
2026-03-04 22:51:19 -0800

    remove sddm

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@
 # Install all dependencies
 pacman -S --needed base-devel linux-headers linux-lts-headers \
     nvidia-utils nvidia-settings mesa-utils nvidia-dkms \
-    xorg-server xorg-xinit sddm \
+    xorg-server xorg-xinit \
     qt6-base qt6-declarative qt6-svg \
-    qt6-multimedia-ffmpeg qt6-virtualkeyboard \
+    qt6-multimedia-ffmpeg  \
     zsh nvim less tree yazi zip unzip \
     rustup nvm lua \
     git github-cli cava opencode \
